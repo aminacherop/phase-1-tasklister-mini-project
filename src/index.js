4dbf60a286a1586f9377a9c183cb435b6782d1c2
aminacherop <cheropamina26@gmail.com>
2025-03-16 13:43:19 +0300

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const taskDescription = taskInput.value.trim();
+    if (taskDescription !== '') {
+      addTask(taskDescription);
+      taskInput.value = '';
+    }
+  });
+
+
+  function addTask(description) {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = description;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    deleteButton.className = 'delete-button';
+    deleteButton.addEventListener('click', function() {
+      taskList.removeChild(taskItem);
+    });
+    
+  
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+  }
+  
 });
+
+
